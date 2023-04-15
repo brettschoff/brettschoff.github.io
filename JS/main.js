412573ba8bbbba3f1bd3ea5a63b0feb8de64b89f
brettschoff <brett.schoff@gmail.com>
2023-04-15 14:33:19 -0600

    Creates page with content and scrolling

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+console.log('Script Loaded')
+
+$('#about-btn').click(function() {
+    $('html,body').animate({
+        scrollTop: $('#about-me').offset().top}, 'slow');
+});
+
+$('#skills-btn').click(function() {
+    $('html,body').animate({
+        scrollTop: $('#skills').offset().top}, 'slow');
+});
+
+$('#contact-btn').click(function() {
+    $('html,body').animate({
+        scrollTop: $('#contact').offset().top}, 'slow');
+});
+$('#portfolio-btn').click(function() {
+    $('html,body').animate({
+        scrollTop: $('.projects').offset().top}, 'slow');
+});
